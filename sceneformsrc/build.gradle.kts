54dc4f32f5301693c07c106af2d3c279effb590f
Kurt Mueller <kurt.mueller@streem.pro>
2020-12-02 12:30:07 -0800

    jcenter-publish - removed commented-out lines in build.gradle.kts

--- sceneformsrc/build.gradle.kts
+++ sceneformsrc/build.gradle.kts
@@ -33,34 +33,3 @@ allprojects {
         mavenLocal()
     }
 }
-
-//task clean()
-
-//buildscript {
-//    ext {
-//        kotlin_version = '1.4.20'
-//    }
-//    repositories {
-//        google()
-//        jcenter()
-//        mavenLocal()
-//    }
-//    dependencies {
-//        classpath 'com.android.tools.build:gradle:4.1.1'
-//        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-//        // NOTE: Do not place your application dependencies here; they belong
-//        // in the individual module build.gradle files
-//    }
-//}
-//
-//allprojects {
-//    repositories {
-//        google()
-//        jcenter()
-//        mavenLocal()
-//    }
-//}
-//
-//task clean(type: Delete) {
-//    delete rootProject.buildDir
-//}
